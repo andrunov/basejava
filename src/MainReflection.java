c3d488e96e6060f2566b89f090634bb33a9ec36f
Андрей <andrunov@gmail.com>
2024-06-26 00:10:18 +0300

    HW4 - reflection

---
+++ src/MainReflection.java
@@ -0,0 +1,15 @@
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Resume resume = new Resume("uuid01");
+        Class<? extends Resume> aClass = resume.getClass();
+        Method toString = aClass.getMethod("toString");
+        Object result = toString.invoke(resume);
+        System.out.println(result);
+    }
+}
