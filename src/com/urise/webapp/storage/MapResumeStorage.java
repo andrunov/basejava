20c5a8be1f5539e36ee40050976ad857afb2e615
Андрей <andrunov@gmail.com>
2024-11-11 16:21:32 +0300

    HW6 MapResumeStorage

--- src/com/urise/webapp/storage/MapResumeStorage.java
+++ src/com/urise/webapp/storage/MapResumeStorage.java
@@ -7,9 +7,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MapResumeStorage extends AbstractStorage<Resume> {
+public class MapResumeStorage extends AbstractStorage<String> {
 
-    protected final Map<Resume, Resume> storage;
+    protected final Map<String, Resume> storage;
 
     public MapResumeStorage() {
         this.storage = new HashMap<>();
@@ -17,36 +17,34 @@ public class MapResumeStorage extends AbstractStorage<Resume> {
 
 
     @Override
-    public Resume doGet(Resume key) {
+    public Resume doGet(String key) {
         return storage.get(key);
     }
 
     @Override
-    public void doSave(Resume key, Resume resume) {
-        storage.put(resume, resume);
+    public void doSave(String key, Resume resume) {
+        storage.put(resume.getUuid(), resume);
     }
 
     @Override
-    public void doUpdate(Resume key, Resume resume) {
-        storage.put(resume, resume);
+    public void doUpdate(String key, Resume resume) {
+        storage.put(key, resume);
     }
 
     @Override
-    public void doDelete(Resume key) {
+    public void doDelete(String key) {
         storage.remove(key);
     }
 
     @Override
-    protected boolean isExist(Resume key) {
-        return storage.containsKey(key);
+    protected boolean isExist(String key) {
+        return key != null;
     }
 
     @Override
-    protected Resume searchKey(String uuid) {
-        for (Resume resume : storage.keySet()){
-            if (resume.getUuid().equals(uuid)) {
-                return resume;
-            }
+    protected String searchKey(String uuid) {
+        if (storage.containsKey(uuid)) {
+            return storage.get(uuid).getUuid();
         }
         return null;
     }
@@ -63,7 +61,7 @@ public class MapResumeStorage extends AbstractStorage<Resume> {
 
     @Override
     public void delete(String uuid) {
-        Resume key = searchKey(uuid);
+        String key = searchKey(uuid);
         storage.remove(key);
     }
 
