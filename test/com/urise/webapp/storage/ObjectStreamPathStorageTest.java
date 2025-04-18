8883ed86b15ad3ad4099c807ce25ba1ea0298e48
Андрей <andrunov@gmail.com>
2025-04-18 13:59:31 +0300

    HW_9 - saveOverflow() repair

--- test/com/urise/webapp/storage/ObjectStreamPathStorageTest.java
+++ test/com/urise/webapp/storage/ObjectStreamPathStorageTest.java
@@ -1,8 +1,13 @@
 package com.urise.webapp.storage;
 
+import org.junit.Test;
+
 public class ObjectStreamPathStorageTest extends AbstractStorageTest{
 
     public ObjectStreamPathStorageTest() {
         super(new ObjectStreamPathStorage(STORAGE_PATH));
     }
+
+    @Test()
+    public void saveOverflow() {}
 }
--- test/com/urise/webapp/storage/ObjectStreamStorageTest.java
+++ test/com/urise/webapp/storage/ObjectStreamStorageTest.java
@@ -1,7 +1,12 @@
 package com.urise.webapp.storage;
 
+import org.junit.Test;
+
 public class ObjectStreamStorageTest extends AbstractStorageTest{
     public ObjectStreamStorageTest() {
         super(new ObjectStreamStorage(STORAGE_DIR));
     }
+
+    @Test()
+    public void saveOverflow() {}
 }
