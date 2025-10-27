cdab6011d2c73a890f41f80e11bcf42ff423366e
Андрей <andrunov@gmail.com>
2025-10-27 12:39:01 +0300

    war deploy

--- config/populate.sql
+++ config/populate.sql
@@ -1,4 +1,9 @@
 INSERT INTO resume (uuid, full_name) VALUES
-    ('7de882da-02f2-4d16-8daa-60660aaf4071', 'Name1'),
-    ('a97b3ac3-3817-4c3f-8a5f-178497311f1d', 'Name2'),
-    ('dd0a70d1-5ed3-479a-b452-d5e04f21ca73', 'Name3');
+    ('3fdf7eac-3025-4d3e-9d2b-64e4143d3880', 'Reserved 1'),
+    ('d24ec8ae-5926-479e-af63-009c15c8527b', 'Reserved 2'),
+    ('b4e1b816-8656-4af0-ab31-47f9d2b884bb', 'Reserved 3'),
+    ('c2602af8-6efa-46d2-9539-af11786e3eaa', 'Reserved 4'),
+    ('4e90c472-6fc1-43f8-b9dc-1840192aa890', 'Reserved 5'),
+    ('7de882da-02f2-4d16-8daa-60660aaf4071', 'Name 1'),
+    ('a97b3ac3-3817-4c3f-8a5f-178497311f1d', 'Name 2'),
+    ('dd0a70d1-5ed3-479a-b452-d5e04f21ca73', 'Name 3');
--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -5,15 +5,15 @@ import com.urise.webapp.storage.SqlStorage;
 import com.urise.webapp.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 
 public class Config {
-    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
+    private static final File PROPS =  new File("/opt/tomcat/latest/conf/resumes.properties");
     private static final Config INSTANCE = new Config();
 
     private final Properties props = new Properties();
@@ -33,9 +33,9 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Files.newInputStream(PROPS.toPath())) {
             props.load(is);
-            storageDir = new File(props.getProperty("storage.dir"));
+            storageDir = null;
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
